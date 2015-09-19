b2552cabdb011f435e9b73e662be1462960632e0
Nihilate <Nihilate@users.noreply.github.com>
2015-09-19 11:33:37 +1000

    Fixed minor bug where Reddit's token expires at a bad point

--- roboragi/Search.py
+++ roboragi/Search.py
@@ -197,7 +197,7 @@ def isBotAParent(comment, reddit):
             
     except:
         traceback.print_exc()
-        return True
+        return False
 
 #Checks if the comment is valid (i.e. not already seen, not a post by Roboragi and the parent commenter isn't Roboragi)
 def isValidComment(comment, reddit):
