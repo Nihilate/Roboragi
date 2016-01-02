35de94cc6f847a95afb53bc8db0e69704d12c98d
Nihilate <Nihilate@users.noreply.github.com>
2016-01-02 11:18:34 +1100

    Changed it to be Python 3 compatible and added another unescape

--- roboragi/MAL.py
+++ roboragi/MAL.py
@@ -106,10 +106,11 @@ def getClosestAnime(searchText, animeList):
         #traceback.print_exc()
         return None
 
-#MAL's XML is a piece of crap.
+#MAL's XML is a piece of crap. It needs to be escaped twice because they do shit like this: &amp;sup2;
 def convertShittyXML(text):
-    import HTMLParser
-    return HTMLParser.HTMLParser().unescape(text)
+    import html
+    text=html.unescape(text)
+    return html.unescape(text)
 
 #Used to check if two descriptions are relatively close. This is used in place of author searching because MAL don't give authors at any point.
 def getClosestFromDescription(mangaList, descriptionToCheck):
